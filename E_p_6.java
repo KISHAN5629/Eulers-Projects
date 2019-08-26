ef5341dc4ec140d69e7666c0d733e5a468f4f1fd
KISHAN5629 <53340946+KISHAN5629@users.noreply.github.com>
2019-08-26 20:31:38 +0530

    Update E_p_6.java

--- E_p_6.java
+++ E_p_6.java
@@ -1,10 +1,9 @@
-
-public class euler_q_6 {
+class euler_q_6 {
 
 	public static void main(String[] args) {
 		System.out.println(new euler_q_6().result());
 	}
-	private static final int N = 100;
+	  int N = 100;
 	
 	public String result() {
 		int sum = 0;
